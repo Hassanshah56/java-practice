3c55bbd90f061d53fc6e0e11965847b2f54042a6
Hassan shah <hassanshah33093@gmail.com>
2025-02-11 20:06:21 +0500

    Add files via upload

---
+++ day_2/day_3/day_4/day_5/date.js
@@ -0,0 +1,16 @@
+
+let newdate = new Date();
+console.log(newdate.toString());
+
+let created = new Date(2004,3,12);
+console.log(created.toLocaleDateString());
+
+
+let date2 = new Date("12-=06-2024")
+console.log(date2.toLocaleDateString());
+
+
+let date = Date.now()
+console.log(date);
+
+console.log(Math.floor(Date.now()/1000));
