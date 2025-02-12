9ca3424229b32d236859e0b0cd31bd3d1531a1e1
Hassan shah <hassanshah33093@gmail.com>
2025-02-12 20:05:47 +0500

    Add files via upload

---
+++ day_2/day_3/day_4/day_5/day_6/java.js
@@ -0,0 +1,70 @@
+// ++++++++++++ ARRAY IN [JAVASCRIPT]++++++++++++++
+
+// let array = [1,2,3,4,5]
+// console.log(array);
+
+// console.log(typeof array);
+// //++++++++++++ [array is object]+++++++++++++++++++
+
+// //++++++ we also find a 1 digit in array 0 to any value like index number 
+// console.log(array[4]);
+
+
+// shallow copy 
+// let orignail = [ a = 1, b=2,i=[c=3]]
+// let  copy = orignail;
+// orignail.i= 80;
+// orignail.a= 70;
+// console.log(orignail);
+// console.log(copy);
+
+// if you change the nested code the make orginal 
+
+
+
+// aray prototype function method 
+
+// let ar = ["fdsrsdrsae","hhhhhhhhh","ikdsaihahtaotnoa","ojasdfaofnoianf"]
+// let jk = ar;
+// // ar.push(7);
+// // ar.push(88);
+// // ar.push(8);
+// // ar.pop(0)
+// // ar.unshift(9)
+// // ar.shift()
+// // console.log(jk);
+
+// // console.log(ar);
+// console.log(ar.indexOf("hhhhhhhhh"));
+
+
+
+// let myarr= [1,2,3,4,5,6]
+// let myaar1 = myarr.slice(1,3)
+// let myaar4 = myarr.splice(1,3)
+// console.log(myarr);
+// console.log(myaar1);
+// console.log(myaar4);
+
+
+// let string = "hassan";
+// let string1 = string.substring(0,3);
+// console.log(string1);
+
+
+// let numf = "hassan";
+// let new2 = numf.substr(0,3)
+// console.log(new2);
+// console.log(numf);
+
+// const str = 'Hello, world!';
+
+// console.log(str.substring(-3, 5)); // 'Hello' (negative startIndex treated as 0)
+// console.log(str.substr(-6, 5)); // 'world' (starts at 6th character from the end, extracts 5 cha
+
+
+// let lower = "HASSAN";
+// console.log(lower.toLowerCase());
+
+// let captal = "hassan";
+// console.log(captal.toUpperCase());
